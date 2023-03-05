bffe3264c44b082d15bf253e9616fe99fe9bd77b
Grizolda <79770415+Grizolda@users.noreply.github.com>
2023-03-05 22:52:06 +0200

    small changes

--- Static Functions & Strings/exc1.cs
+++ Static Functions & Strings/exc1.cs
@@ -5,15 +5,15 @@
             for(int i = 0; i < 20; i++){
                 int firstNum = random.Next(1, 101);
                 int secondNum = random.Next(1, 101);
-                Console.WriteLine($"\n({firstNum}, {secondNum})");
+                Console.WriteLine($"\n({firstNum}, {secondNum}):");
                 
                 if(firstNum <= secondNum){
                     for(int j = firstNum; j <= secondNum; j++){
-                        Console.Write(j + " ");
+                        Console.Write(j + ",");
                     }
-                }else if(firstNum >= secondNum){
+                }else{
                     for(int j = secondNum; j <= firstNum; j++){
-                        Console.Write(j + " ");
+                        Console.Write(j + ",");
                     }
                 }
             }
