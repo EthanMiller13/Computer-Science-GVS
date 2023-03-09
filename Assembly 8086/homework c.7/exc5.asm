459173615ba6de8dd637af0de0bc7e4cdaf1821b
Grizolda <79770415+Grizolda@users.noreply.github.com>
2023-03-09 19:06:16 +0200

    exc5 answered

--- Assembly 8086/homework c.7/exc5.asm
+++ Assembly 8086/homework c.7/exc5.asm
@@ -1,9 +1,47 @@
 .MODEL small
 .STACK
 .DATA        
-    msg1 db 13, 10, 'Please insert a digit: '
+    msg1 db 13, 10, 'Please insert a digit: $'
+    isADigit db 13, 10, 'The number is: $'
+    notADigit db 13, 10, 'The char is not a number: $'
     char db ?
 .CODE
-  
+    mov ax, @data
+    mov ds, ax
+    
+    lea dx, msg1
+    mov ah, 09h
+    int 21h
+    
+    mov ah, 01h
+    int 21h
+    mov char, al
+    
+    ; if not a digit - jump to label "not_a_digit"
+    cmp char, 48
+    jb not_a_digit
+    
+    cmp char, 57
+    ja not_a_digit
+    
+    ; otherwise, if char is digit - print the output
+    lea dx, isADigit
+    mov ah, 09h
+    int 21h
+    
+    lea dx, char
+    mov ah, 09h
+    int 21h
+    jmp exit
+    
+not_a_digit:
+    lea dx, notADigit
+    mov ah, 09h
+    int 21h
+       
+    
+exit:
+    mov ah, 4Ch
+    int 21h    
 END
 
